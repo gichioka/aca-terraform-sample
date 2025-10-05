c6d51ee2a5260985b58dc06c48b6cd5ea17bb54e
gichioka <stbkx3695@yahoo.co.jp>
2025-10-05 17:29:36 +0900

    コミットメッセージ

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,3 +89,19 @@ resource "azurerm_container_app" "app" {
 output "container_app_url" {
   value = azurerm_container_app.app.latest_revision_fqdn
 }
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "aca_name" {
+  value = azurerm_container_app.app.name
+}
